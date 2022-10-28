7e465b358d0e62326d66d237eefffb4f08dd31a8
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-10-28 10:41:10 +0100

    fixing cors issue

--- SlackDetail/views.py
+++ SlackDetail/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import SlackDetail
 from .serializer import DetailSerializer
+from rest_framework.decorators import api_view
 
-def Detail(request):
+@api_view(["GET"])
+def Detail(request, *args, **kwargs):
+    header = {"Access-Control-Allow-Origin":"*"}
     detail = SlackDetail.objects.all()
     serializer = DetailSerializer(detail, many=True)
     return JsonResponse(serializer.data, safe=False)
