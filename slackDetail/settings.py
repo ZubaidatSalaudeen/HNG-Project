128cf7985f35263989fb4509235f9314dad7c626
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-10-28 07:41:45 +0100

    fixing cors issue

--- slackDetail/settings.py
+++ slackDetail/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1:8000', '.herokuapp.com']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = ['127.0.0.1:8000', '.herokuapp.com']
 # Application definition
 
